0ee7b03520d32946dec6c5b75776dc8fd3fea9ef
Hegel <sobotafran@gmail.com>
2025-11-02 17:18:41 +0100

    tmux integration

--- init.lua
+++ init.lua
@@ -105,14 +105,6 @@ vim.keymap.set('n', '<right>', '<cmd>echo "Use l to move!!"<CR>')
 vim.keymap.set('n', '<up>', '<cmd>echo "Use k to move!!"<CR>')
 vim.keymap.set('n', '<down>', '<cmd>echo "Use j to move!!"<CR>')
 
--- Keybinds to make split navigation easier.
---  Use CTRL+<hjkl> to switch between windows
---
---  See `:help wincmd` for a list of all window commands
-vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left window' })
-vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
-vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
-vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
 
 -- NOTE: Some terminals have colliding keymaps or are not able to send distinct keycodes
 -- vim.keymap.set("n", "<C-S-h>", "<C-w>H", { desc = "Move window to the left" })
@@ -225,6 +217,7 @@ require('lazy').setup({
   require 'custom.plugins.lualine',
   require 'custom.plugins.gruvbox-material',
 
+  require 'custom.plugins.vim-tmux-navigator',
   -- Highlight todo, notes, etc in comments
   require 'kickstart_default.plugins.todo-comments',
 
--- lua/lsp/plugins/blink.lua
+++ lua/lsp/plugins/blink.lua
@@ -58,6 +58,10 @@ return {
       -- See :h blink-cmp-config-keymap for defining your own keymap
       preset = 'default',
 
+      -- Prevent colision with tmux prefix 
+      ['<C-space>'] = {},
+      ['<C-h>'] = { 'show', 'show_documentation', 'hide_documentation' },
+
       -- For more advanced Luasnip keymaps (e.g. selecting choice nodes, expansion) see:
       --    https://github.com/L3MON4D3/LuaSnip?tab=readme-ov-file#keymaps
     },
